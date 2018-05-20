e43e9f075ea9ea278d72bcb3b5ff7605d3778ee1
mikhio <mikhail.iogansen@gmail.com>
2018-05-20 22:42:03 +0300

    add actions

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ var div = document.querySelector('.res');
 var find = function (cl, num) {
 	var el = document.querySelector('.' + cl);
 	var ch = function() {
-		div.textContent = num;
+		if (num === '+' || num === '-' || num === '*' || num === '/') {
+			div.textContent += num;
+		} else if (cl === 'delet' && div.textContent.length > 1) {
+			div.textContent = div.textContent.slice(0, -1);
+		} else if (cl === 'delet') {
+			div.textContent = num;
+		} else if (cl === 'clear') {
+			div.textContent = num
+		} else if (div.textContent === '0') {
+			div.textContent = num;
+		} else {
+			div.textContent += num;
+		}
 	};
 	el.addEventListener('click', ch);
 };
@@ -17,3 +29,11 @@ find('six', 6)
 find('seven', 7)
 find('eight', 8)
 find('nine', 9)
+find('null', 0)
+find('clear', 0);
+find('delet', 0);
+find('del', '/');
+find('um', '*');
+find('minus', '-');
+find('plus', '+');
+//find('equally', =);
